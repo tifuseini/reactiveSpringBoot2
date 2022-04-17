13125d8bd28529bf4c6057864a7236afbf6f9e40
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-17 15:50:53 +0000

    Added tomcat anf Post not working

--- src/main/java/com/springboot/reactiveSpringBoot2/controller/ApiController.java
+++ src/main/java/com/springboot/reactiveSpringBoot2/controller/ApiController.java
@@ -4,13 +4,14 @@ import com.springboot.reactiveSpringBoot2.model.Image;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.codec.multipart.FilePart;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
@@ -31,16 +32,15 @@ public class ApiController {
     }
 
     @PostMapping(API_BASE_PATH + "/images")
-    Mono<Void> create(@RequestParam Flux<FilePart> images){
+    Mono<Void> create(@RequestPart Flux<FilePart> images) {
         Hooks.onOperatorDebug();
 
         return images
                 .map(image -> {
-                    log.info("We will save " +image + "to a Reactive database soon!");
+                    log.info("We will save " + image + " to a Reactive database soon!");
                     return image;
                 })
                 .then();
-
     }
 
 }
