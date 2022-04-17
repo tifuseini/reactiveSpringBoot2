7e1f330d2febd24f30d0f3ae8e8720631d2d25f0
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-17 15:04:44 +0000

    Image Class Update

--- src/main/java/com/springboot/reactiveSpringBoot2/model/Image.java
+++ src/main/java/com/springboot/reactiveSpringBoot2/model/Image.java
@@ -1,4 +1,19 @@
 package com.springboot.reactiveSpringBoot2.model;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Image {
+
+    private String id;
+
+    private String name;
+
+    public Image(String id,String name){
+        this.id = id;
+        this.name = name;
+    }
 }
