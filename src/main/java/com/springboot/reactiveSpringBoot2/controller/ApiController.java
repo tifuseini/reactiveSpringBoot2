66aa3be1f52884be73ca8d991b9cbda8bbdd4698
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-17 15:12:50 +0000

    Added API BASE URL AND LOGGER'

--- src/main/java/com/springboot/reactiveSpringBoot2/controller/ApiController.java
+++ src/main/java/com/springboot/reactiveSpringBoot2/controller/ApiController.java
@@ -1,7 +1,14 @@
 package com.springboot.reactiveSpringBoot2.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
+
+    private static final Logger log = LoggerFactory.getLogger(ApiController.class);
+
+    private static final String API_BASE_PATH = "/api/v1";
+
 }
