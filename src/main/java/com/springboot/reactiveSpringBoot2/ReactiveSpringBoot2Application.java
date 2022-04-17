e5e0311d3d7d11628907eb557ed522fe2b1a0c4b
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-17 10:04:22 +0000

    Source Folder

---
+++ src/main/java/com/springboot/reactiveSpringBoot2/ReactiveSpringBoot2Application.java
@@ -0,0 +1,13 @@
+package com.springboot.reactiveSpringBoot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringBoot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/reactiveSpringBoot2/ReactiveSpringBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.reactiveSpringBoot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringBoot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
