6fd8cd708548e0e45b7b1332e964ddeaa5f5c757
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-17 19:37:55 +0000

    ResourceLoader

--- src/main/java/com/springboot/reactiveSpringBoot2/service/ImageService.java
+++ src/main/java/com/springboot/reactiveSpringBoot2/service/ImageService.java
@@ -1,4 +1,17 @@
 package com.springboot.reactiveSpringBoot2.service;
 
+import com.springboot.reactiveSpringBoot2.model.Image;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ImageService {
+
+    private static final String UPLOAD_ROOT = "upload-dir";
+
+    private final ResourceLoader resourceLoader;
+
+    public ImageService(ResourceLoader resourceLoader){
+        this.resourceLoader = resourceLoader;
+    }
 }
