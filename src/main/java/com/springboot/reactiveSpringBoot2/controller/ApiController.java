bdf52374bc81de1fe958b4d598d63f21d73778a4
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-17 15:23:27 +0000

    @Post Mapping abd Mono

--- src/main/java/com/springboot/reactiveSpringBoot2/controller/ApiController.java
+++ src/main/java/com/springboot/reactiveSpringBoot2/controller/ApiController.java
@@ -3,10 +3,14 @@ package com.springboot.reactiveSpringBoot2.controller;
 import com.springboot.reactiveSpringBoot2.model.Image;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class ApiController {
@@ -24,6 +28,18 @@ public class ApiController {
                 new Image(2,"Image 2"),
                 new Image(3,"Image 4")
         );
+    }
+
+    @PostMapping(API_BASE_PATH + "/images")
+    Mono<Void> create(@RequestParam Flux<FilePart> images){
+        Hooks.onOperatorDebug();
+
+        return images
+                .map(image -> {
+                    log.info("We will save " +image + "to a Reactive database soon!");
+                })
+                .then();
+
     }
 
 }
