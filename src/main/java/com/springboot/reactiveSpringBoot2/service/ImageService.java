da235c7ba0c33d13c5d54ac17a1c25926e72ca1f
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-17 19:44:53 +0000

    Mock Images

--- src/main/java/com/springboot/reactiveSpringBoot2/service/ImageService.java
+++ src/main/java/com/springboot/reactiveSpringBoot2/service/ImageService.java
@@ -1,8 +1,18 @@
 package com.springboot.reactiveSpringBoot2.service;
 
 import com.springboot.reactiveSpringBoot2.model.Image;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.FileSystemUtils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Service
 public class ImageService {
@@ -13,5 +23,23 @@ public class ImageService {
 
     public ImageService(ResourceLoader resourceLoader){
         this.resourceLoader = resourceLoader;
+    }
+
+    @Bean
+    CommandLineRunner setUp() throws IOException{
+        return args -> {
+            FileSystemUtils.deleteRecursively(new File(UPLOAD_ROOT));
+
+            Files.createDirectory(Paths.get(UPLOAD_ROOT));
+
+            FileCopyUtils.copy("Test File",
+                    new FileWriter(UPLOAD_ROOT + "image1"));
+
+            FileCopyUtils.copy("Test File",
+                    new FileWriter(UPLOAD_ROOT + "image2"));
+
+            FileCopyUtils.copy("Test File",
+                    new FileWriter(UPLOAD_ROOT + "image3"));
+        };
     }
 }
