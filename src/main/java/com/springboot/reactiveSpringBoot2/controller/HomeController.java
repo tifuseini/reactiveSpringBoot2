fa79f9c5184b4590b1dca10b61528d00e8e40eb5
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-18 20:19:13 +0000

    Home Controller

--- src/main/java/com/springboot/reactiveSpringBoot2/controller/HomeController.java
+++ src/main/java/com/springboot/reactiveSpringBoot2/controller/HomeController.java
@@ -1,5 +1,18 @@
 package com.springboot.reactiveSpringBoot2.controller;
 
+import com.springboot.reactiveSpringBoot2.service.ImageService;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class HomeController {
 
+    private static final String BASE_PATH = "/images";
+
+    private static final String FILENAME = "{filename:.+}";
+
+    private final ImageService imageService;
+
+    public HomeController(ImageService imageService){
+        this.imageService = imageService;
+    }
 }
