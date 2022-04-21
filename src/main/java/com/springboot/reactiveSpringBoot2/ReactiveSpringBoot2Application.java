2e013d52464eccd1a18d75591c1d60d8e87f0063
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-21 21:21:30 +0000

    ReactiveSpringBoot2Application update

--- src/main/java/com/springboot/reactiveSpringBoot2/ReactiveSpringBoot2Application.java
+++ src/main/java/com/springboot/reactiveSpringBoot2/ReactiveSpringBoot2Application.java
@@ -2,12 +2,19 @@ package com.springboot.reactiveSpringBoot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class ReactiveSpringBoot2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveSpringBoot2Application.class, args);
+	}
+
+	@Bean
+	HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+		return new HiddenHttpMethodFilter();
 	}
 
 }
